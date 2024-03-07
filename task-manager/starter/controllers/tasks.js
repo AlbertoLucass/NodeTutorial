49bbffe6786a4034101b64ff7cdde3b8fe0ce2d7
Alberto Lucas da Trindade Neto <alberto.trindade@ufba.br>
2024-03-07 15:21:35 -0300

    Setting the routes

--- task-manager/starter/controllers/tasks.js
+++ task-manager/starter/controllers/tasks.js
@@ -1,8 +1,24 @@
 const getAllTasks= (req, res) => {
-    res.send('all items');
+    res.send('get all items');
 }
 
+const createTask = (req, res) => {
+    res.send('create a new task')
+}
+const getTask = (req, res) => {
+    res.send('get a single task')
+}
+const updateTask = (req, res) => {
+    res.send('update a task')
+}
+const deleteTask = (req, res) => {
+    res.send('delete a task')
+}
 
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 };
