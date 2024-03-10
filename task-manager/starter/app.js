6f5101984cb94884c6a94f333ecfcdc3b84470bf
Alberto Lucas da Trindade Neto <alberto.trindade@ufba.br>
2024-03-10 14:54:00 -0300

    End of the main logic

--- task-manager/starter/app.js
+++ task-manager/starter/app.js
@@ -6,13 +6,12 @@ require('dotenv').config();
 
 // middleware
 
+app.use(express.static('./public'));
 app.use(express.json());
 
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-});
+
 
 app.use('/api/v1/tasks', tasks)
 //app.get('/api/v1/tasks) - get all the tasks
