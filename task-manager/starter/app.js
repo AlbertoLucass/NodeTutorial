fb1d6649569364b887c820fd53ad34bc9e08398f
Alberto Lucas da Trindade Neto <alberto.trindade@ufba.br>
2024-03-10 15:35:05 -0300

    End of the task manager project

--- task-manager/starter/app.js
+++ task-manager/starter/app.js
@@ -3,22 +3,19 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('../starter/db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 
 app.use(express.static('./public'));
+app.use(errorHandlerMiddleware);
 app.use(express.json());
-
 
 // routes
 
-
-app.use('/api/v1/tasks', tasks)
-//app.get('/api/v1/tasks) - get all the tasks
-//app.post('/api/v1/tasks) - create a new task
-//app.get('/api/v1/tasks/:id) - get a single task
-//app.patch('/api/v1/tasks/:id) - update a task
-//app.delete('/api/v1/tasks/:id) - delete a task
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
 const port = 3000;
 
